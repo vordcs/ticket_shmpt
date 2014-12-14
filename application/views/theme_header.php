dc881c59de6b663d3eece0bc781d8836a1480ad2
VoRD <vord@VoRDs-MacBook-Pro.local>
2014-12-14 11:21:39 +0700

    back up to word pc

--- application/views/theme_header.php
+++ application/views/theme_header.php
@@ -65,38 +65,26 @@
         </script>
     </head>
     <body>
-        <!-- Fixed navbar -->
-        <div class="navbar navbar-fixed-top sh-nav" role="navigation">
-            <div id="top-nav" class="sh-top-nav">          
-                <h4 class="color-white" style="margin: 4px 0px 4px 4px;">บริษัท สหมิตรภาพ(2512) จำกัด</h4>
-                <p class="text-right" style="margin-top: -20px;">
-                    <a id="popoverMail" href="#" data-content="ติดต่อกับผู้ให้บริการ ผ่าน support@thaihubhosting.com" rel="popover" data-placement="bottom" data-original-title="ติดต่อผ่านอีเมล์" data-trigger="hover"><i class="fa fa-envelope"></i> Support |</a>
-                    <a id="popoverFacebook" class="hidden-xs" href="#" data-content="ติดต่อกับผู้ให้บริการ ผ่าน Facebookpage ThaiHubHosting" rel="popover" data-placement="bottom" data-original-title="ติดต่อผ่านเพจ" data-trigger="hover"> <i class="fa fa-facebook"></i> ThaiHubHosting |</a>
-                    <a id="popoverMobile" href="#" data-content="ติดต่อกับผู้ให้บริการ ผ่านเบอร์ <?= lang('phone_number1') ?>" rel="popover" data-placement="bottom" data-original-title="ติดต่อผ่านสายตรง" data-trigger="hover"><i class="fa fa fa-mobile-phone"></i> <?= lang('phone_number1') ?></a>
-                </p>
-            </div>
-            <div class="navbar-collapse collapse">
-                <div class="subnavbar">
-                    <div class="subnavbar-inner">
-                        <div class="container" id="mainmenu">
-                            <ul class="mainnav">
-                                <li class="active"><a href="<?= base_url('home/') ?>"><i class="fa fa-dashboard"></i><span>Dashboard</span> </a> </li>
-                                <li id="btnCandidate"><a href="<?= base_url('candidate/') ?>"><i class="fa fa-bullhorn"></i><span>สมัครงาน</span> </a> </li>
-                                <li id="btnHR"><a href="<?= base_url('hr/') ?>"><i class="fa fa-users"></i><span>งานบุคคล</span> </a> </li>
-                                <li id="btnVehicle"><a href="<?= base_url('vehicle/') ?>"><i class="fa fa-car"></i><span>รถ</span> </a> </li>
-                                <li id="btnRoute"><a href="<?= base_url('route/') ?>"><i class="fa fa-share-alt"></i><span>เส้นทาง</span></a></li>
-                                <li id="btnSchedule"><a href="<?= base_url('schedule/') ?>"><i class="fa fa-list"></i><span>ตารางเดินรถ</span></a></li>
-                                <li id="btnCost"><a href="<?= base_url('cost/') ?>"><i class="fa fa-pencil-square-o"></i><span>ค่าใช้จ่าย</span></a></li>
-                                <li id="btnReport"><a href="<?= base_url('report/') ?>"><i class="fa fa-calendar-o"></i><span>รายงาน</span> </a> </li>                              
-                            </ul>
-                        </div>
-                        <!-- /container --> 
-                    </div>
-                    <!-- /subnavbar-inner --> 
+        <nav class="navbar navbar-fixed-top sh-nav" role="navigation">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Project name</a>
                 </div>
-            </div>
-            <!--/.nav-collapse -->
-        </div>
+                <div id="navbar" class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </div><!-- /.nav-collapse -->
+            </div><!-- /.container -->
+        </nav><!-- /.navbar -->
 
         <?php
         if (isset($debug) && $debug != NULL) {
@@ -108,16 +96,13 @@
         }
 
         if (isset($alert) && $alert != NULL) {
-            if ($alert['alert_mode'] == 'success'){
+            if ($alert['alert_mode'] == 'success') {
                 echo '<div class="container alert alert-success animated pulse" style="margin-top: 60px;"><strong>สำเร็จ</strong> ';
-            }
-            elseif ($alert['alert_mode'] == 'warning'){
+            } elseif ($alert['alert_mode'] == 'warning') {
                 echo '<div class="container alert alert-warning animated pulse" style="margin-top: 60px;"><strong>คำเตือน</strong> ';
-            }
-            elseif ($alert['alert_mode'] == 'danger'){
+            } elseif ($alert['alert_mode'] == 'danger') {
                 echo '<div class="container alert alert-danger animated pulse" style="margin-top: 60px;"><strong>ผิดพลาด</strong> ';
-            }
-            else{
+            } else {
                 echo '<div class="container alert alert-info animated pulse" style="margin-top: 60px;"><strong>เพิ่มเติม</strong> ';
             }
             echo $alert['alert_message'];
